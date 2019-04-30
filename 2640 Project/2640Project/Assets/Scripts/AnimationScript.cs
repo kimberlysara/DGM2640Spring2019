a678fa3d9f7047693d9b699974d11183f1e8376c
Unknown <kimberlyshopkins99@gmail.com>
2019-04-29 21:20:11 -0600

    work on stuff

---
+++ 2640 Project/2640Project/Assets/Scripts/AnimationScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationScript : MonoBehaviour
+{
+	public Animator Anim;
+
+	// Use this for initialization
+	void Start()
+	{
+		Anim = GetComponent<Animator>();
+		//Anim.StopPlayback();
+		Anim.gameObject.GetComponent<Animator>().enabled = false;
+
+	}
+
+	void AnimStart()
+	{
+		//Anim.gameObject.GetComponent<Animator>().enabled = true;
+		Anim.gameObject.SetActive(true);
+	}
+
+}
